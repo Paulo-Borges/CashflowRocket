8201a1c0d2be92eb9954a88b0f10f70127c13191
Borges <borgespaulo72@yahoo.com.br>
2026-09-14 17:52:35 -0300

    feat: Criando EndPoint de despesas

---
+++ src/CashFlowRocket.API/Controllers/ExpensesController.cs
@@ -0,0 +1,16 @@
+﻿using CashFlowRocket.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlowRocket.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisterExpensesJson request)
+        {
+            return Created();
+        }
+    }
+}
